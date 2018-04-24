cc8a18af38d5ee9c8e05a16109e2df80e551f3e5
uvec_fukumori <uvec_fukumori@DESKTOP-KG5A238>
2018-04-24 17:53:23 +0900

    180424

--- src/practice/fundamentals/chapter0/No006.java
+++ src/practice/fundamentals/chapter0/No006.java
@@ -1,8 +1,19 @@
 package practice.fundamentals.chapter0;
+
 /*
  * mainメソッド内にRunnebleインターフェースを実装したローカルクラスを作成し、
  * 「インターフェース実装のローカルクラス」と表示させるプログラムを作成してください。
  */
 public class No006 {
-
+	public static void main(String[] args) {
+		 class Thread implements Runnable{
+				public void run() {
+					System.out.println("インターフェース実装のローカルクラス");
+			}
+		 }
+		 Thread a=new Thread();
+		 a.run();
+	}
 }
+
+
--- src/practice/fundamentals/chapter0/No008.java
+++ src/practice/fundamentals/chapter0/No008.java
@@ -1,4 +1,4 @@
-package fundamentals.chapter0;
+package practice.fundamentals.chapter0;
 /*
  * Runnableインターフェースを使用して、「Hello ラムダ」が表示されるようにプログラムを作成してください。
  * ※ラムダ式を使用すること
